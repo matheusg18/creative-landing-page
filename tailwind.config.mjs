d5e9306841a87cef7b317c592b4346f7e518a408
Matheus Santos <matheusgabriel.18@gmail.com>
2024-09-13 00:42:53 -0300

    feat: add project specifics font sizes to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,36 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontSize: {
+      base: [
+        '18px',
+        {
+          lineHeight: '32px',
+          fontWeight: '400',
+        },
+      ],
+      xl: [
+        '20px',
+        {
+          lineHeight: '32px',
+          fontWeight: '800',
+        },
+      ],
+      '2xl': [
+        '56px',
+        {
+          lineHeight: '64px',
+          fontWeight: '800',
+        },
+      ],
+      '3xl': [
+        '80px',
+        {
+          lineHeight: '88px',
+          fontWeight: '800',
+        },
+      ],
+    },
     extend: {},
   },
   plugins: [],
