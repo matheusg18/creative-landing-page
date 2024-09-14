cd2673168a323b84df0d8d62398d4ea5db052c0e
Matheus Santos <matheusgabriel.18@gmail.com>
2024-09-14 15:43:20 -0300

    chore: remove custom tailwind theme config and add the project font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,38 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    fontSize: {
-      base: [
-        '18px',
-        {
-          lineHeight: '32px',
-          fontWeight: '400',
-        },
-      ],
-      xl: [
-        '20px',
-        {
-          lineHeight: '32px',
-          fontWeight: '800',
-        },
-      ],
-      '2xl': [
-        '56px',
-        {
-          lineHeight: '64px',
-          fontWeight: '800',
-        },
-      ],
-      '3xl': [
-        '80px',
-        {
-          lineHeight: '88px',
-          fontWeight: '800',
-        },
-      ],
+    extend: {
+      fontFamily: {
+        sans: ['Commissioner', ...defaultTheme.fontFamily.sans],
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
